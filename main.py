ff56c959950558badd8126fa11495d2afe460b72
arkopaulhalder <halderarkopaul@gmail.com>
2024-07-08 21:27:20 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from CNN_Classifier import logger
 from CNN_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from CNN_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -9,6 +9,18 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>>>>stage {STAGE_NAME} completed<<<<<\n\n<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+        logger.info(f"************************")
+        logger.info(f">>>>>>> STAGE {STAGE_NAME} STARTED <<<<<<<<<<<<<<<<<")
+        prepare_base_model = PrepareBaseModelTrainingPipeline()
+        prepare_base_model.main()
+        logger.info(f">>>>>>STAGE {STAGE_NAME} COMPLETED <<<<<<< \n \n ******************")
 except Exception as e:
     logger.exception(e)
     raise e
---
+++ src/CNN_Classifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,28 @@
+from CNN_Classifier.config.configuration import ConfigurationManager
+from CNN_Classifier.components.prepare_base_model import PrepareBaseModel
+from CNN_Classifier import logger
+
+STAGE_NAME = "Prepare base model"
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"************************")
+        logger.info(f">>>>>>> STAGE {STAGE_NAME} STARTED <<<<<<<<<<<<<<<<<")
+        OBJ = PrepareBaseModelTrainingPipeline()
+        OBJ.main()
+        logger.info(f">>>>>>STAGE {STAGE_NAME} COMPLETED <<<<<<< \n \n ******************")
+    except Exception as e:
+        logger.exception(e)
+        raise e
